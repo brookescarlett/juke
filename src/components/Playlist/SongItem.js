ae958e48b0535466eaba536e00ef66441bd618ac
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-04-28 15:09:24 -0400

    playlist styled

--- src/components/Playlist/SongItem.js
+++ src/components/Playlist/SongItem.js
@@ -51,8 +51,8 @@ class SongItem extends Component {
         <div className="bottom-row">
           <div><p>song artist: {this.props.datum.artist}</p></div>
           <div className="votes">
-            <p onClick={this.handleUpVote}>upvote: {this.props.datum.upVote}</p>
-            <p onClick={this.handleDownVote}>downVote: {this.props.datum.downVote}</p>
+            <p onClick={this.handleUpVote}><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-thumbs-up"><path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path></svg>{this.props.datum.upVote}</p>
+            <p onClick={this.handleDownVote}><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-thumbs-down"><path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17"></path></svg>{this.props.datum.downVote}</p>
           </div>
         </div>
       </div>
