582d0b95cff0691e915e7e07939fdfe98687d305
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-04-29 12:03:05 -0400

    filter working nicely

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -12,7 +12,6 @@ class Filter extends Component {
 
   constructor(props) {
     super(props)
-
     this.state = {
       searchResults: []
     }
@@ -33,6 +32,7 @@ class Filter extends Component {
       .then(res => res.json())
       .then(json => {
 
+
         this.props.ToggleModal(true)
 
         json.tracks !== undefined ?
@@ -42,6 +42,7 @@ class Filter extends Component {
 
       })
     } else {
+      this.props.ToggleModal(false)
       this.setState({
         searchResults: []
       })
@@ -55,7 +56,7 @@ class Filter extends Component {
   }
 
   componentDidMount = () => {
-    if (this.props.ToggleModal(true)) {
+    if (this.props.displayModal === true) {
       window.addEventListener('keydown', this.listenKeyboard, true)
     }
   }
@@ -87,13 +88,42 @@ class Filter extends Component {
       <div>
 
         <input type="text" onChange={this.handleChange} placeholder="Search" className="filter-bar"></input>
-
-        {this.props.displayModal ?
+        {this.props.displayModal === true ?
         <div>
-          <div className="modal-overlay-div"  />
+          <div className="modal-overlay-div" onClick={this.onOverlayClick} />
           <div className="modal-content-div" onClick={this.onOverlayClick}>
             <div className="modal-dialog-div"  onClick={this.onDialogClick}>
-            {this.state.searchResults !== [] ? this.renderSearchResults() : null}
+            {this.state.searchResults !== [] ?
+
+              (<div><svg width="22" height="32" viewBox="0 0 55 80" xmlns="http://www.w3.org/2000/svg" fill="#FFF">
+                  <g transform="matrix(1 0 0 -1 0 80)">
+                      <rect width="10" height="20" rx="3">
+                          <animate attributeName="height"
+                               begin="0s" dur="4.3s"
+                               values="20;45;57;80;64;32;66;45;64;23;66;13;64;56;34;34;2;23;76;79;20" calcMode="linear"
+                               repeatCount="indefinite" />
+                      </rect>
+                      <rect x="15" width="10" height="80" rx="3">
+                          <animate attributeName="height"
+                               begin="0s" dur="2s"
+                               values="80;55;33;5;75;23;73;33;12;14;60;80" calcMode="linear"
+                               repeatCount="indefinite" />
+                      </rect>
+                      <rect x="30" width="10" height="50" rx="3">
+                          <animate attributeName="height"
+                               begin="0s" dur="1.4s"
+                               values="50;34;78;23;56;23;34;76;80;54;21;50" calcMode="linear"
+                               repeatCount="indefinite" />
+                      </rect>
+                      <rect x="45" width="10" height="30" rx="3">
+                          <animate attributeName="height"
+                               begin="0s" dur="2s"
+                               values="30;45;13;80;56;72;45;76;34;23;67;30" calcMode="linear"
+                               repeatCount="indefinite" />
+                      </rect>
+                  </g>
+              </svg>{this.renderSearchResults()}</div>)
+               : null}
             </div>
           </div>
         </div> : null }
